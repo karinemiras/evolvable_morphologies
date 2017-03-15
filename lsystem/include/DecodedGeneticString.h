5e53f1a12629d2693e536f9a76c78929b34ccac3
Karine Miras <karine.smiras@gmail.com>
2017-03-15 10:38:05 +0100

    Added missing header file DecodedGeneticString.h

---
+++ lsystem/include/DecodedGeneticString.h
@@ -0,0 +1,44 @@
+//
+// Created by Karine Miras on 08/03/2017.
+//
+
+#ifndef LSYSTEM_PROTO_DECODEDGENOME_H
+#define LSYSTEM_PROTO_DECODEDGENOME_H
+
+#include <string>
+
+
+#include "GeneticString.h"
+#include "LSystem.h"
+
+class DecodedGeneticString{
+
+
+public:
+
+
+    struct node{
+        std::string item;
+        int id;
+        struct node *left;
+        struct node *front;
+        struct node *right;
+        struct node *back; // parent component in the tree
+    };
+
+    DecodedGeneticString::node * getRoot();
+
+    void decode(GeneticString gs, LSystem LS);
+
+    DecodedGeneticString(){
+        root = NULL;
+    }
+
+
+private:
+
+    node *root = new node;
+
+};
+
+#endif //LSYSTEM_PROTO_DECODEDGENOME_H
