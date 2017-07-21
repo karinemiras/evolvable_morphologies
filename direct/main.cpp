47719724d10036297d248c99e172f43d8a8b6380
karinemiras <karine.smiras@gmail.com>
2017-07-21 20:12:15 +0200

    mpi

--- direct/main.cpp
+++ direct/main.cpp
@@ -6,6 +6,8 @@
 //  Copyright (c) 2015 Berend Weel. All rights reserved.
 //
 
+#include "mpi.h"
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -30,19 +32,42 @@
 
 using namespace robogen;
 
+
+
 int main(int argc,  char* argv[]) {
 
+    // BEGIN: parallel repetitions of experiments
 
-    // experiment: looking for global optimum of innovation
-    int niche_coverage_accumulated = 0;
+        int rc, num_tasks , my_rank;
+        rc = MPI_Init( &argc, &argv);
 
-    Evolution * e = new Evolution("direct", 1);
-    // 1-indirect TYPE is temporary, it will be replaced by genome parent class scheme
-    niche_coverage_accumulated = e->noveltySearch(argc, argv, 1);
+        if (rc != MPI_SUCCESS) {
+            fprintf(stderr, "Unable to set up MPI");
+            MPI_Abort(MPI_COMM_WORLD, rc);
+        }
 
-    std::cout<<">>> accumulated niche coverage " << niche_coverage_accumulated;
+        MPI_Comm_size( MPI_COMM_WORLD , &num_tasks);
+        MPI_Comm_rank( MPI_COMM_WORLD , &my_rank);
 
 
+        // runs experiment
+        int niche_coverage_accumulated = 0;
+        Evolution * e = new Evolution("direct"+std::to_string(my_rank), 1);
+        niche_coverage_accumulated = e->noveltySearch(argc, argv, 1);
+
+        std::cout<<">>> experiment "<<std::to_string(my_rank)<<", accumulated niche coverage " << niche_coverage_accumulated<<std::endl;
+
+
+        MPI_Finalize ();
+
+    // END: parallel repetitions of experiments
+
+
+
+//    int niche_coverage_accumulated = 0;
+//    Evolution * e = new Evolution("direct_Test", 1);
+//    niche_coverage_accumulated = e->noveltySearch(argc, argv, 1);
+//    std::cout<<">>> experiment, accumulated niche coverage " << niche_coverage_accumulated<<std::endl;
 
     return 0;
 }
--- indirect/lsystem/main.cpp
+++ indirect/lsystem/main.cpp
@@ -8,20 +8,46 @@
 #include<iostream>
 
 #include "EvolutionIndirect.h"
-#include "Tuning.h"
+
+#include "mpi.h"
+
 
 
 int main(int argc, char* argv[]) {
 
 
-     // experiment: looking for global optimum of innovation
-     int niche_coverage_accumulated = 0;
 
-     EvolutionIndirect * e = new EvolutionIndirect("indirect", 1);
-     // 1-indirect TYPE is temporary, it will be replaced by genome parent class scheme
-     niche_coverage_accumulated = e->noveltySearch(argc, argv, 1);
+    // BEGIN: parallel repetitions of experiments
 
-     std::cout<<">>> accumulated niche coverage " << niche_coverage_accumulated;
+        int rc, num_tasks , my_rank;
+        rc = MPI_Init( &argc, &argv);
+
+        if (rc != MPI_SUCCESS) {
+            fprintf(stderr, "Unable to set up MPI");
+            MPI_Abort(MPI_COMM_WORLD, rc);
+        }
+
+        MPI_Comm_size( MPI_COMM_WORLD , &num_tasks);
+        MPI_Comm_rank( MPI_COMM_WORLD , &my_rank);
+
+
+        // runs experiment
+        int niche_coverage_accumulated = 0;
+        EvolutionIndirect * e = new EvolutionIndirect("indirect"+std::to_string(my_rank), 1);
+        niche_coverage_accumulated = e->noveltySearch(argc, argv, 1);
+
+        std::cout<<">>> experiment "<<std::to_string(my_rank)<<", accumulated niche coverage " << niche_coverage_accumulated<<std::endl;
+
+
+        MPI_Finalize ();
+
+    // END: parallel repetitions of experiments
+
+
+//     int niche_coverage_accumulated = 0;
+//     EvolutionIndirect * e = new EvolutionIndirect("indirect", 1);
+//     niche_coverage_accumulated = e->noveltySearch(argc, argv, 1);
+//     std::cout<<">>> accumulated niche coverage " << niche_coverage_accumulated;
 
 
     // e->testGeneticString(argc, argv,"spider3");
